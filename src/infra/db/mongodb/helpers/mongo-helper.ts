ad7cddb30af5e4f279bddec6108a050d6e815e49
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-02-03 22:43:03 -0300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -14,5 +14,9 @@ export const MongoHelper = {
 
   getCollection (name: string): Collection {
     return this.client.db().collection(name)
+  },
+
+  map: (collection: any, id: string): any => {
+    return { id, ...collection }
   }
 }
