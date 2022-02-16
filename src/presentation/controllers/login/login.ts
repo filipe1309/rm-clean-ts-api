5cbc98dd24933b33ed68d93bb7684540c9713b43
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-02-16 20:27:19 -0300

    feat: ensure LoginController returns 500 if EmailValidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest, ok } from '../../helpers/http-helpers'
+import { badRequest, ok, serverError } from '../../helpers/http-helpers'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { EmailValidator } from '../signup/signup-protocols'
 
@@ -7,17 +7,21 @@ export class LoginController implements Controller {
   constructor (private readonly emailValidator: EmailValidator) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body
-    if (!email) {
-      return await new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
+    try {
+      const { email, password } = httpRequest.body
+      if (!email) {
+        return await new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
+      }
+      if (!password) {
+        return await new Promise(resolve => resolve(badRequest(new MissingParamError('password'))))
+      }
+      const isValidEmail = this.emailValidator.isValid(email)
+      if (!isValidEmail) {
+        return await new Promise(resolve => resolve(badRequest(new InvalidParamError('email'))))
+      }
+      return await new Promise(resolve => resolve(ok(httpRequest.body)))
+    } catch (error) {
+      return serverError(error)
     }
-    if (!password) {
-      return await new Promise(resolve => resolve(badRequest(new MissingParamError('password'))))
-    }
-    const isValidEmail = this.emailValidator.isValid(email)
-    if (!isValidEmail) {
-      return await new Promise(resolve => resolve(badRequest(new InvalidParamError('email'))))
-    }
-    return await new Promise(resolve => resolve(ok(httpRequest.body)))
   }
 }
