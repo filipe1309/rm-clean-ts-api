ae90584f81bc2bedda95511d2876b736bb1abb36
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-02-20 23:01:36 -0300

    test: add tests to CompareFieldsValidation

--- src/presentation/helpers/validators/compare-fields-validation.ts
+++ src/presentation/helpers/validators/compare-fields-validation.ts
@@ -9,7 +9,7 @@ export class CompareFieldsValidation implements Validation {
 
   validate (input: any): Error | null {
     if (input[this.fieldName] !== input[this.fieldToCompareName]) {
-      return new InvalidParamError(this.fieldName)
+      return new InvalidParamError(this.fieldToCompareName)
     }
     return null
   }
