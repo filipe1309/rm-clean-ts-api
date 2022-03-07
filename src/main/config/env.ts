e4697860201fe75b3735078e5bf0f060bc7b7114
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-03-06 23:11:50 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://root:12345@localhost:27017/clean-node-api?authSource=admin',
-  port: process.env.PORT ?? 5050
+  port: process.env.PORT ?? 5050,
+  jwtSecret: process.env.JWT_SECRET ?? 'tj670==5H'
 }
