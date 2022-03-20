8e5699a935dca93d72b55f1407f7a7d8aa0c285d
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-03-20 16:54:23 -0300

    test: add AddSurvey composition

---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,42 @@
+import { Collection } from 'mongodb'
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+
+let surveyCollection: Collection
+
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    const mongoUrl = process.env.MONGO_URL ?? 'mongodb://localhost:27017/test'
+    await MongoHelper.connect(mongoUrl)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  describe('POST /surveys', () => {
+    test('Should return 204 on add survey success', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: 'Question',
+          answers: [
+            {
+              answer: 'Answer 1',
+              image: 'http://my-image.com/img.jpg'
+            },
+            {
+              answer: 'Answer 2'
+            }
+          ]
+        })
+        .expect(204)
+    })
+  })
+})
