7961f486ab70e36ee5348c36acba1bef32a1067d
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-03-27 20:08:15 -0300

    feat: ensure add survey route return 403 if no access token is provided

---
+++ src/main/adapters/express-middleware-adapter.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, RequestHandler, Response } from 'express'
+import { HttpRequest, Middleware } from '../../presentation/protocols'
+
+export const adaptMiddleware = (middleware: Middleware): RequestHandler => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const httpRequest: HttpRequest = {
+      headers: req.headers
+    }
+    const httpReponse = await middleware.handle(httpRequest)
+    if (httpReponse.statusCode === 200) {
+      Object.assign(req, httpReponse.body)
+      next()
+    } else {
+      res.status(httpReponse.statusCode).json({
+        error: httpReponse.body.message
+      })
+    }
+  }
+}
