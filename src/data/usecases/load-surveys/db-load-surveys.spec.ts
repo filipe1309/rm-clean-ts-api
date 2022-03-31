e037d16cfed8e9f4a507ba3451dfab51e2427825
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-03-31 20:49:03 -0300

    test: ensure DbLoadSurveys calls LoadSurveysRepository

---
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -0,0 +1,38 @@
+import { SurveyModel } from '../../../domain/models/survey'
+import { LoadSurveysRepository } from '../../protocols/db/survey/load-surveys-repository'
+import { DbLoadSurveys } from './db-load-surveys'
+
+const makeFakeSurveys = (): SurveyModel[] => {
+  return [{
+    id: 'any_id',
+    question: 'any_question',
+    answers: [{
+      image: 'any_image',
+      answer: 'any_answer'
+    }],
+    date: new Date()
+  }, {
+    id: 'any_other_id',
+    question: 'any_other_question',
+    answers: [{
+      image: 'any_other_image',
+      answer: 'any_other_answer'
+    }],
+    date: new Date()
+  }]
+}
+
+describe('DbLoadSurveys', () => {
+  test('Should call LoadSurveysRepository', async () => {
+    class LoadSurveysRepositoryStub implements LoadSurveysRepository {
+      async loadAll (): Promise<SurveyModel[]> {
+        return await new Promise(resolve => resolve(makeFakeSurveys()))
+      }
+    }
+    const loadSurveysRepositoryStub = new LoadSurveysRepositoryStub()
+    const sut = new DbLoadSurveys(loadSurveysRepositoryStub)
+    const loadAllSpy = jest.spyOn(loadSurveysRepositoryStub, 'loadAll')
+    await sut.load()
+    expect(loadAllSpy).toHaveBeenCalled()
+  })
+})
