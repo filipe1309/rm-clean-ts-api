57477fc83ec50930137f055736cbf778e622feea
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-04-06 20:55:32 -0300

    fix: make the error message instead of the error name

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -7,7 +7,7 @@ export default (app: Express): void => {
   app.use('/api', router)
   const pathToRoutes = path.join(__dirname, '..', 'routes')
   readdirSync(pathToRoutes).map(async file => {
-    if (!file.includes('.test.')) {
+    if (!file.includes('.test.') && !file.endsWith('.map')) {
       (await import(`../routes/${file}`)).default(router)
     }
   })
