bff9f951da78af736bdee583b06cb09a3c77977d
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-04-10 19:05:57 -0300

    test: ensure DbSaveSurveyResult returns a survey result if SaveSurveyResultRepository succeeds

--- src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
+++ src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -59,4 +59,10 @@ describe('DbSaveSurveyResult Usecase', () => {
     const promise = sut.save(makeFakeSurveyResultData())
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return SurveyResult on success', async () => {
+    const { sut } = makeSut()
+    const surveyResult = await sut.save(makeFakeSurveyResult())
+    expect(surveyResult).toEqual(makeFakeSurveyResult())
+  })
 })
