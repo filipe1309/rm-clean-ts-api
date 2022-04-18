cada996f49dca3bce5c64792f0dc98e3a0f7384e
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-04-18 20:07:19 -0300

    refactor: change middleware paths to use module

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -1,5 +1,5 @@
+import app from '@/main/config/app'
 import request from 'supertest'
-import app from '../config/app'
 
 describe('Cors Middleware', () => {
   test('Should enable cors', async () => {
