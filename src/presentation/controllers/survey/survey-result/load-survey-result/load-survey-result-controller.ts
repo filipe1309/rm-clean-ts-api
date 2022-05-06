2c2e535a6e24f4ddd1965be24d44d356e8106abc
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-05-05 22:01:19 -0300

    feat: ensure LoadSurveyResultController calls LoadSurveyResult with correct values

--- src/presentation/controllers/survey/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -1,16 +1,21 @@
 import { InvalidParamError } from '@/presentation/errors'
 import { forbidden, serverError } from '@/presentation/helpers/http/http-helpers'
-import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from './load-survey-result-controller-protocols'
+import { Controller, HttpRequest, HttpResponse, LoadSurveyById, LoadSurveyResult } from './load-survey-result-controller-protocols'
 
 export class LoadSurveyResultController implements Controller {
-  constructor (private readonly loadSurveyByIdStub: LoadSurveyById) {}
+  constructor (
+    private readonly loadSurveyByIdStub: LoadSurveyById,
+    private readonly lloadSurveyResultStub: LoadSurveyResult
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const survey = await this.loadSurveyByIdStub.loadById(httpRequest.params.surveyId)
+      const { surveyId } = httpRequest.params
+      const survey = await this.loadSurveyByIdStub.loadById(surveyId)
       if (!survey) {
         return forbidden(new InvalidParamError('surveyId'))
       }
+      await this.lloadSurveyResultStub.load(surveyId)
       return null
     } catch (error) {
       return serverError(error)
