51810b95adc4f7e279b99cfb4c5a40a951a16adc
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-07-09 19:05:13 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -5,6 +5,7 @@ export const surveyResultPath = {
     }],
     tags: ['Survey'],
     summary: 'API to create survey answer',
+    description: 'This route if for authenticated users to create survey answer',
     parameters: [{
       in: 'path',
       name: 'surveyId',
@@ -22,6 +23,43 @@ export const surveyResultPath = {
         }
       }
     },
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  },
+  get: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Survey'],
+    summary: 'API to load survey result',
+    description: 'This route if for authenticated users to load survey result',
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
     responses: {
       200: {
         description: 'Success',
