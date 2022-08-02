562392ed89af4a2953ad3fb3933cc84a4b6064c1
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-08-02 20:05:43 -0300

    refactor: LoadSurveyById use Case

--- tests/data/usecases/db-load-survey-by-id.spec.ts
+++ tests/data/usecases/db-load-survey-by-id.spec.ts
@@ -42,7 +42,7 @@ describe('DbLoadSurveyById', () => {
   test('Should return Survey on success', async () => {
     const { sut, loadSurveyByIdRepositorySpy } = makeSut()
     const survey = await sut.loadById(surveyId)
-    expect(survey).toEqual(loadSurveyByIdRepositorySpy.surveyModel)
+    expect(survey).toEqual(loadSurveyByIdRepositorySpy.result)
   })
 
   test('Should throw if LoadSurveyByIdRepository throws', async () => {
